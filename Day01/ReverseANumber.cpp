30892d11ad87b0b9fcc3037e31756305eedcf6c7
PradipSaha9153 <pradipkrsaha9153@gmail.com>
2026-07-02 19:03:35 +0530

    Add Reverse a Number Program

---
+++ Day01/ReverseANumber.cpp
@@ -0,0 +1,25 @@
+// Reverse a Number
+
+#include<iostream>
+using namespace std;
+
+int reverse(int num) {
+    int rev = 0;
+    while(num != 0) {
+        rev = rev * 10 + num % 10;
+        num /= 10;
+    }
+
+    return rev;
+}
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "After reverse the number: " << reverse(num);
+
+    return 0;
+}
