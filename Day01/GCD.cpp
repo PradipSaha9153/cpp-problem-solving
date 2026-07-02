5bee373f0f8c6cc768e5f6ec4f81434ccee9ed79
PradipSaha9153 <pradipkrsaha9153@gmail.com>
2026-07-02 19:30:29 +0530

    Add GCD of two numbers

---
+++ Day01/GCD.cpp
@@ -0,0 +1,28 @@
+// Greatest Common Divisor (GCD) of two numbers
+
+#include<iostream>
+using namespace std;
+
+int gcd(int n1, int n2) {
+    int GCD;
+
+    for(int i = 1; i < n1 && i < n2; i++) {
+        if(n1 % i == 0 && n2 % i == 0)
+            GCD = i;
+    }
+
+    return GCD;
+}
+
+int main() {
+    int n1, n2;
+
+    cout << "Enter the 1st number: ";
+    cin >> n1;
+    cout << "Enter the 2nd number: ";
+    cin >> n2;
+
+    cout << "Greatest Common Divisor (GCD) of two numbers: " << gcd(n1, n2);
+
+    return 0;
+}
