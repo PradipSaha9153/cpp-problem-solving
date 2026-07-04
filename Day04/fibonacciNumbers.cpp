f53df0218837a558164f11641b8384e03d0c2169
PradipSaha9153 <pradipkrsaha9153@gmail.com>
2026-07-04 12:50:11 +0530

    Add a program that print a fibonacci series

---
+++ Day04/fibonacciNumbers.cpp
@@ -0,0 +1,31 @@
+// Program For Fibonacci Numbers
+
+#include<iostream>
+using namespace std;
+
+void fibonacci(int n) {
+    if(n == 0) {
+        cout << "Fibonacci series of " << n << " is: 0";
+        return;
+    }
+    int a = 0, b = 1;
+
+    cout << "Fibonacci series of " << n << " is: " << endl;
+    cout << a << " " << b << " ";
+    for(int i = 2; i <= n; i++) {
+        cout << a + b << " ";
+        int t = a;
+        a = b;
+        b = t + b;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    fibonacci(n);
+
+    return 0;
+}
