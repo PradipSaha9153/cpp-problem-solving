05e2864b59c3d56275fe7472abd6c59f24c72158
Pradip Kumar Saha <pradipkrsaha9153@gmail.com>
2026-07-06 23:15:46 +0530

    Add a Program To Print Right Half Pyramid Pattern

---
+++ Day06/RightHalfPyramid.cpp
@@ -0,0 +1,14 @@
+// Program To Print Right Half Pyramid Pattern
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    for(int i = 1; i <= 5; i++){
+        for(int j = 1; j <= i; j++)
+            cout << "*";
+        cout << endl;
+    }
+
+    return 0;
+}
