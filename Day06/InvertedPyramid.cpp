a9687a862f073caec7275fa1bea82ab76a139e79
Pradip Kumar Saha <pradipkrsaha9153@gmail.com>
2026-07-06 23:57:34 +0530

    Add space between *

--- Day06/InvertedPyramid.cpp
+++ Day06/InvertedPyramid.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     for(int i = 5; i >= 1; i--) {
         for(int j = 1; j <= i; j++)
-            cout << "*";
+            cout << "* ";
         cout << endl;
     }
 }
