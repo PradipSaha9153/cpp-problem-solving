48f0dbd1a2f6482627119d8793d7e6ff96df0336
Pradip Kumar Saha <pradipkrsaha9153@gmail.com>
2026-07-06 23:37:38 +0530

    Add a Program To Print Left Half Pyramid Pattern

---
+++ Day06/LeftHalfPyramid.cpp
@@ -0,0 +1,17 @@
+// Program To Print Left Half Pyramid Pattern
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i = 1; i <= 5; i++) {
+        for(int j = 1; j <= 5; j++) {
+            if(j >= 6 - i && j <= 5)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
