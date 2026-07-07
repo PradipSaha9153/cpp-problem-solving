ca1e0b5c0a85164c78f13a5df37de2cbc0d65c9d
Pradip Kumar Saha <pradipkrsaha9153@gmail.com>
2026-07-07 23:25:26 +0530

    Add a Program To Calculate the Power of a Number

---
+++ Day07/PowerUsingRecursion.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int poweOfANumber(int b, int p) {
+    if(p == 0)
+        return 1;
+    if(p == 1)
+        return b;
+
+    return b * poweOfANumber(b, p - 1);
+}
+
+int main() {
+    int base;
+    int power;
+    
+    cout << "Enter the number: ";
+    cin >> base;
+    cout << "Enter the power: ";
+    cin >> power;
+
+    cout << base << "^" << power << " = " << poweOfANumber(base, power) << endl;
+
+    return 0;
+}
