d0532092609855a4cf9cb1f51b47ac7dba08127d
Pradip Kumar Saha <pradipkrsaha9153@gmail.com>
2026-07-09 22:18:25 +0530

    Add a Program For Average of an Array (Iterative)

---
+++ Day09/AverageOfAnArray.cpp
@@ -0,0 +1,28 @@
+// Program For Average of an Array (Iterative)
+
+#include<iostream>
+using namespace std;
+
+float average(int arr[], int size) {
+    int sum = 0;
+    for(int i = 0; i < size; i++)
+        sum += arr[i];
+
+    return (sum / (float)size);
+}
+
+int main() {
+    int n;
+
+    cout << "Enter array size: ";
+    cin >> n;
+    int arr[n];
+
+    cout << "Enter array element: " << endl;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "Average of all elements is: " << average(arr, n) << endl;
+
+    return 0;
+}
