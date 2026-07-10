fe98d36a67a813450f8ad3ea8d289d39b46c2ba5
Pradip Kumar Saha <pradipkrsaha9153@gmail.com>
2026-07-10 23:48:34 +0530

    Add a program that demonstrate multilevel inheritance

---
+++ Day10/multilevelInheritance.cpp
@@ -0,0 +1,35 @@
+// Program that demonstrate single level inheritance
+
+#include<iostream>
+using namespace std;
+
+class A {
+public:
+    void a() {
+        cout << "Class A function a" << "\n";
+    }
+};
+
+class B : public A {
+public:
+    void b() {
+        cout << "Class B function b" << "\n";
+    }
+};
+
+class C : public B {
+public:
+    void c() {
+        cout << "Class C function c" << "\n";
+    }
+};
+
+int main() {
+    C obj;
+
+    obj.a(); // Print class A function a
+    obj.b(); // Print class b function b
+    obj.c(); // Print class b function b
+
+    return 0;
+}
