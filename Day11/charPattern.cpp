010a7e60dd5154334e971c207d030ea6cef483f9
Pradip Kumar Saha <pradipkrsaha9153@gmail.com>
2026-07-11 16:41:32 +0530

    Add a character pattern program

---
+++ Day11/charPattern.cpp
@@ -0,0 +1,22 @@
+/*
+ABCD
+ABCD
+ABCD
+ABCD
+*/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    for(int i = 0; i < 4; i++){
+        char ch = 'A';
+        for(int j = 0; j < 4; j++){
+            cout << ch;
+            ch++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
