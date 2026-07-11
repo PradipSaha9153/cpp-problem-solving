50eccb67fcb886bbdab2a8e6ab8c58cf47fbfae6
Pradip Kumar Saha <pradipkrsaha9153@gmail.com>
2026-07-12 02:00:29 +0530

    Add a program that demonstrate bitwise operators

---
+++ Day012/bitwiseOperators.cpp
@@ -0,0 +1,14 @@
+// Program that demonstrate bitwise operators
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "Bitwise AND(&): " << (4 & 8) << endl;
+    cout << "Bitwise OR(|): " << (4 | 8) << endl;
+    cout << "Bitwise XOR(^): " << (4 ^ 8) << endl;
+    cout << "Bitwise Left Shift Operator(<<): " << (8 << 1) << endl;
+    cout << "Bitwise Right Shift Operator(>>): " << (10 >> 2) << endl;
+
+    return 0;
+}
