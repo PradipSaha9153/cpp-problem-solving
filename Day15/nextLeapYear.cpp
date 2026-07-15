32df62537fb2985980d6d2da0d61cb20efc2cc08
Pradip Kumar Saha <pradipkrsaha9153@gmail.com>
2026-07-15 23:57:12 +0530

    Add a progam that find is it leap year if no find next leap year - Method01

---
+++ Day15/nextLeapYear.cpp
@@ -0,0 +1,24 @@
+/*
+Input1: An integer value N representing the given year.
+
+Output: Return an integer value representing the next nearest leap year for a given year.
+*/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int year;
+    cout << "Enter a year: ";
+    cin >> year;
+
+    int i = 1;
+    do {
+        if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            cout << "Leap year: " << year << endl;
+            return 0;
+        }
+        year++;
+        i++;
+    } while(i <= 5);
+}
