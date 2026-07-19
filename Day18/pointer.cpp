e8bb514833385ebfb0e02547a1b31b232878034d
Pradip Kumar Saha <pradipkrsaha9153@gmail.com>
2026-07-19 19:38:19 +0530

    Example of Dereference Operator(*)

--- Day18/pointer.cpp
+++ Day18/pointer.cpp
@@ -3,9 +3,17 @@ using namespace std;
 
 int main() {
     // Initialization
+    /*
     int x = 10;
     int* ptr = &x;
     
     cout << &x << endl;
     cout << ptr << endl;
+    */
+
+    // Dereference Operator(*)
+    int x = 10;
+    int* ptr = &x;
+    
+    cout << *ptr << endl;
 }
