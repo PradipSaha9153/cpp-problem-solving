e2fd0f8cfb5146c2ea0ac627ef3524dad6bb4e0b
Pradip Kumar Saha <pradipkrsaha9153@gmail.com>
2026-07-19 20:11:44 +0530

    Example of pointer to pointer

--- Day18/pointer.cpp
+++ Day18/pointer.cpp
@@ -20,10 +20,23 @@ int main() {
     */
 
     // Changing value using pointer
+    /*
     int x = 10;
     int* ptr = &x;
 
     *ptr = 50;
 
     cout << x << endl;
+    */
+
+    // Pointer to Pointer
+    int x = 10;
+    int* ptr = &x;
+    int** ptrPtr = &ptr;
+
+    cout << &ptr << endl;     // 0x7ffd82afd2a8
+    cout << ptrPtr << endl;   // 0x7ffd82afd2a8
+    cout << ptr << endl;      // 0x7ffd82afd2a4
+    cout << *ptrPtr << endl;  // 0x7ffd82afd2a4
+    cout << **ptrPtr << endl; // 10
 }
