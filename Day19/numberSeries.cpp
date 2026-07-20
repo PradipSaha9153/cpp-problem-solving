60a7b94c552c5824755b313773d79fb99431a21f
Pradip Kumar Saha <pradipkrsaha9153@gmail.com>
2026-07-20 10:04:42 +0530

    Add explanation for this series

--- Day19/numberSeries.cpp
+++ Day19/numberSeries.cpp
@@ -1,3 +1,15 @@
+/*
+For n = 5, the series starts at 2 and increases in the following pattern:
+
+1st term: 2 (Start)
+2nd term: 2 + 13 = 15
+3rd term: 15 + (2 * 13) = 41
+4th term: 41 + (3 * 13) = 80
+5th term: 80 + (4 * 13) = 132
+
+Thus, the output is 2 ,15 ,41, 80 ,132.
+*/
+
 #include<iostream>
 using namespace std;
 
