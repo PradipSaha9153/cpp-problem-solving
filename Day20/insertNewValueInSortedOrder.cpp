3affd058b46eb86c137501c4ae792b25d9d37ba9
Pradip Kumar Saha <pradipkrsaha9153@gmail.com>
2026-07-21 11:28:52 +0530

    Add a program that insert  New Value In Sorted Order

---
+++ Day20/insertNewValueInSortedOrder.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter array size: ";
+    cin >> n;
+    vector<int> arr(n - 1);
+    
+    cout << "Enter array values: " << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    int newValue;
+    cout << "Enter new value: ";
+    cin >> newValue;
+
+    int i = n - 1;
+
+    while (i >= 0 && arr[i] > newValue) {
+        arr[i + 1] = arr[i];
+        i--;
+    }
+
+    arr[i + 1] = newValue;
+
+    for (int j = 0; j <= n; j++) {
+        cout << arr[j];
+        if (j < n) {
+            cout << " ";
+        }
+    }
+
+    cout << endl;
+
+    return 0;
+}
