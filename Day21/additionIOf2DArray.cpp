7b0994e162a96ff57e57687955dcf7a072964f48
Pradip Kumar Saha <pradipkrsaha9153@gmail.com>
2026-07-22 23:32:55 +0530

    Add a program that claculate sum of 2D array

---
+++ Day21/additionIOf2DArray.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int n, m;
+    cout << "Enter two number for size of an array : " << endl;
+    cin >> n >> m;
+    
+    int arr1[n][m];
+    int arr2[n][m];
+    cout << "\nEnter matrix 1 value: " << endl;
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++) 
+            cin >> arr1[i][j];
+        
+    cout << "\nEnter matrix 2 value: " << endl;
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++) 
+            cin >> arr2[i][j];
+
+    int sum[n][m];
+    cout << "\nThe matrix is: " << endl;
+    for(int i = 0; i < n; i++) 
+        for(int j = 0; j < m; j++) 
+            sum[i][j] = arr1[i][j] + arr2[i][j];
+
+    cout << "\nResult = " << endl;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
